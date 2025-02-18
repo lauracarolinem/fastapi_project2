98c2352c07cd8b117af2ed5b26597646f7895338
Laura <llauraccarolinemmenezes@gmail.com>
2025-02-18 04:31:10 -0300

    rota de login

--- app/test/routes/test_user_routes.py
+++ app/test/routes/test_user_routes.py
@@ -30,3 +30,42 @@ def test_register_user_route_user_already_exists(user_on_db):
     response = client.post('/user/register', json=body)
     
     assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
+def test_user_login_route(user_on_db):
+    body = {
+        'username': user_on_db.username,
+        'password': 'pass#'
+    }
+    
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    
+    response = client.post('/user/login', data=body, headers=headers)
+    
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert 'access_token' in data 
+    assert 'expires_at' in data 
+    
+def test_user_login_route_invalid_username(user_on_db):
+    body = {
+        'username': 'invalid',
+        'password': 'pass#'
+    }
+    
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    
+    response = client.post('/user/login', data=body, headers=headers)
+    
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+def test_user_login_route_invalid_password(user_on_db):
+    body = {
+        'username': user_on_db.username,
+        'password': 'Invalid'
+    }
+    
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    
+    response = client.post('/user/login', data=body, headers=headers)
+    
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
