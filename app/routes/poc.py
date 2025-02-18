f8bb5a216fadcafd5f35eb7797fcdf802c3cc369
Laura <llauraccarolinemmenezes@gmail.com>
2025-02-18 10:40:50 -0300

    desacoplando a poc das routes

--- app/routes/poc.py
+++ app/routes/poc.py
@@ -4,20 +4,16 @@ from sqlalchemy.orm import Session
 from app.routes.deps import get_db_session
 from app.schemas.category import CategoryOutput
 from app.db.models import Category as CategoryModel
-from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage
+from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage, Params, LimitOffsetParams
 from fastapi_pagination.ext.sqlalchemy import paginate as sqlalchemy_paginate
+from app.use_cases.poc import list_categories_uc
 
 router = APIRouter(prefix='/poc', tags=['POC'])
 
 @router.get('/list', response_model=Page[CategoryOutput]) #gera todas as paginas igualmente
 @router.get('/list/limit-offset', response_model=LimitOffsetPage[CategoryOutput]) #limit offset tem dois parametros na documentação. Limit=quantos, offset=a partir de qual
-def list_categories():
-    categories = [
-        CategoryOutput(name=f'category {n}', slug=f'category-{n}', id=n)
-        for n in range(100)
-    ]
-    
-    return paginate(categories)
+def list_categories(page: int=1, size: int=50):
+    return list_categories_uc(page=page, size=size)
 
 @router.get('/list/sqlalchemy', response_model=Page[CategoryOutput]) #gera todas as paginas igualmente
 @router.get('/list/limit-offset/sqlalchemy', response_model=LimitOffsetPage[CategoryOutput]) #limit offset tem dois parametros na documentação. Limit=quantos, offset=a partir de qual
